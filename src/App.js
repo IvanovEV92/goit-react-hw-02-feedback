76afb8897d212e4bf39709b2177c24b5dc1bdae8
Ivanov Evhenii <ivanov191192@gmail.com>
2021-03-26 01:29:19 +0200

    complete 1 issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Statistics from './components/Statistics';
+import Notification from './components/Notification';
+import FeedbackOptions from './components/FeedbackOptions';
+import Section from './components/Section';
+class App extends Component {
+	state = {
+		good: 0,
+		neutral: 0,
+		bad: 0,
+	};
+	handlerBTn = ({ target: { textContent } }) => {
+		this.setState({
+			[textContent]: this.state[textContent] + 1,
+		});
+	};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	countTotalFeedback = () => {
+		const total = Object.values({ ...this.state }).reduce((acc, value) => {
+			acc += value;
+			return acc;
+		}, 0);
+		return total;
+	};
+	countPositiveFeedbackPercentage = () => {
+		return Math.round((this.state.good * 100) / this.countTotalFeedback());
+	};
+	render() {
+		const buttons = Object.keys({ ...this.state });
+		return (
+			<>
+				<Section title="Please leave feedback">
+					<FeedbackOptions options={buttons} onLeaveFeedback={this.handlerBTn} />
+				</Section>
+
+				<Section title="Statistics">
+					{this.countTotalFeedback() > 0 ? (
+						<Statistics
+							good={this.state.good}
+							neutral={this.state.neutral}
+							bad={this.state.bad}
+							total={this.countTotalFeedback()}
+							positivePercentage={this.countPositiveFeedbackPercentage()}
+						/>
+					) : (
+						<Notification message="No feedback given" />
+					)}
+				</Section>
+			</>
+		);
+	}
 }
 
 export default App;
